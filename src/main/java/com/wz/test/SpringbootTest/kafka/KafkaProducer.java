62b4cd10c46e5e56b524910fb615219dacc2bdef
wangzheng <wangzhenghdu@163.com>
2019-06-18 20:54:38 +0800

    3

--- src/main/java/com/wz/test/SpringbootTest/kafka/KafkaProducer.java
+++ src/main/java/com/wz/test/SpringbootTest/kafka/KafkaProducer.java
@@ -20,15 +20,22 @@ public class KafkaProducer {
 
     private int num = 0;
 
-    //@Scheduled(fixedRate = 15*1000)
+    @Scheduled(fixedRate = 20*60*1000)
     public void send(){
 
-        String message = "message"+num;
+        for (int i=0;i<2000;i++){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    String message = "message"+num;
+                    num++;
 
-        String key = ""+num;
+                    String key = ""+num%50;
+                    kafkaTemplate.send(TOPIC, key, message);
+                }
+            }).start();
+        }
 
-        kafkaTemplate.send(TOPIC, key, message);
-        num++;
     }
 
 }
--- src/main/java/com/wz/test/SpringbootTest/kafka/MyPartitioner.java
+++ src/main/java/com/wz/test/SpringbootTest/kafka/MyPartitioner.java
@@ -1,6 +1,15 @@
 package com.wz.test.SpringbootTest.kafka;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.kafka.clients.producer.Partitioner;
 import org.apache.kafka.common.Cluster;
 import org.springframework.stereotype.Component;
@@ -15,15 +24,60 @@ public class MyPartitioner implements Partitioner {
 
     private static final String TOPIC = "muti-partition-test";
 
-    @Override
-    public int partition(String s, Object o, byte[] bytes, Object o1, byte[] bytes1,
-            Cluster cluster) {
-        int partitionNum = cluster.partitionCountForTopic(TOPIC);
-        String key = (String) o;
-        int intKey = Integer.parseInt(key);
-        String value = (String) o1;
+    private static Map<String,Integer> keys = new ConcurrentHashMap<>();
 
-        return intKey%partitionNum;
+    private static Map<Integer, AtomicInteger> partitions = new ConcurrentHashMap<>();
+
+    private static AtomicInteger test = new AtomicInteger(0);
+    /**
+     *
+     * 根据ip做负载均衡，同一个ip每次需要发送到同一个partition
+     * */
+    @Override
+    public int partition(String s, Object key, byte[] bytes, Object value, byte[] bytes1,
+            Cluster cluster) {
+
+       // System.out.println("partition.....");
+
+        int partitionNum = cluster.partitionCountForTopic(TOPIC);
+
+        if (partitions.size()!=partitionNum){
+            synchronized (MyPartitioner.class){
+                if (partitions.size()<partitionNum){
+                    for (int i=partitions.size();i<partitionNum;i++){
+                        partitions.put(i,new AtomicInteger(0));
+                    }
+                }
+
+                if (partitions.size()>partitionNum){
+                    rebalance();
+                }
+            }
+        }
+        String keyStr = (String)key;
+        Integer partition = keys.get(key);
+
+        if (partition==null){
+            synchronized (keyStr.intern()){
+                if ((partition=keys.get(key))==null){
+                    System.out.println("partition==null :"+test.getAndIncrement());
+                    List<Entry<Integer, AtomicInteger>> list = new ArrayList<>(partitions.entrySet());
+                    //升序排序
+                    Collections.sort(list, Comparator.comparingInt(o -> o.getValue().intValue()));
+                    Entry<Integer,AtomicInteger> entry = list.get(0);
+                    partition = entry.getKey();
+                    entry.getValue().getAndIncrement();
+                    keys.put(keyStr,partition);
+                }
+            }
+        }
+        //System.out.println("key:"+key+", partition:"+partition);
+
+        return partition;
+    }
+
+    private void rebalance() {
+        //todo
     }
 
     @Override
@@ -35,4 +89,5 @@ public class MyPartitioner implements Partitioner {
     public void configure(Map<String, ?> map) {
 
     }
+
 }
